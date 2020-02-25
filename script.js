335497f6b9944f305e9b3040fc95c64bef0ae1dd
Yannick Couillin <emma.imamiah@gmail.com>
2020-02-25 14:09:41 +0100

    ajout js carousel tourne

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 var caroussel = document.querySelector('.carousel');
 
+var prev = document.querySelector('.prev')
+var next = document.querySelector('.next')
 
+prev.addEventListener("click", function (){
+    rotate(60);
+})
 
-carousel.style.transform = "rotateY(-60deg)";
+next.addEventListener("click", function (){
+    rotate(-60)
+})
+
+function rotate(direction){
+    position += direction;
+    carousel.style.transform = "rotate(" + position + "deg)"
+}
+
 console.log(carousel);
